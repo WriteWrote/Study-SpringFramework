f7cbc2c30a08c3f31688a204da44348a61bafb55
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2021-04-11 17:10:46 +0300

    Scopes 1.1: Prototype&Singletone

--- springApp1/src/main/java/ru/ellinaobukhova/springcourse/TestSpring.java
+++ springApp1/src/main/java/ru/ellinaobukhova/springcourse/TestSpring.java
@@ -8,18 +8,17 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        // Music music = context.getBean("classicalMusicBean", Music.class);
-
         MusicPlayer player1 = context.getBean("musicPlayerBean", MusicPlayer.class);
+        player1.setVolume(100);
         System.out.println("Player " + player1.getName() + ", volume: " + player1.getVolume());
-        player1.playMusic();
+        //player1.playMusic();
 
         MusicPlayer player2 = context.getBean("musicPlayerBean", MusicPlayer.class);
         boolean res = player1 == player2;
         System.out.println("Equal by link: " + res);
-        
-        System.out.println("Player " + player1.getName() + ", volume: " + player1.getVolume());
-        player1.playMusic();
+
+        System.out.println("Player " + player2.getName() + ", volume: " + player2.getVolume());
+        //player2.playMusic();
 
         context.close();
     }
